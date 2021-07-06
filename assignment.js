fd8343a549eff3242e000a27da400aefafa68731
milesdredd <87034849+milesdredd@users.noreply.github.com>
2021-07-06 23:43:00 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -4,3 +4,11 @@ for (i =1 ; i<6; i=i+1){
 	r.push("*")}
 	
 console.log(r)
+
+
+
+
+
+//my first assignment , but we can't print elements in same line just with help of console.log , 
+//so my elder brother suggested to try to make a list of all items and then print that  and use "push" to add items in list , 
+//so i did that 

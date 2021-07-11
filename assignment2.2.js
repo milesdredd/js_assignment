ac98fa709319dd8e11d109d04ee7af240a659cfb
milesdredd <87034849+milesdredd@users.noreply.github.com>
2021-07-11 21:09:45 +0530

    Add files via upload

---
+++ assignment2.2.js
@@ -0,0 +1,19 @@
+
+s="madam is walking with her mom and dad"
+s = s + " "
+word =  " "
+revword = " "
+for(k of s){
+	if(k != "  "){
+		word=word+k
+		revword = k + revword
+	}
+	else{
+		if (word == revword){
+			console.log(word)
+		}
+		word=""
+		revword = ""
+	}
+}
+console.log("hi")
